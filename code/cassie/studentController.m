84b37b0de1441119b21797f29be93cd1634c9b2e
Isabel Paredes <iparedes314@gmail.com>
2018-12-14 16:35:11 -0800

    Added wrench function

--- code/cassie/studentController.m
+++ code/cassie/studentController.m
@@ -9,10 +9,12 @@ function tau = studentController(t, s, model, params)
     % State vector components ID
     q = s(1 : model.n);
     dq = s(model.n+1 : 2*model.n);
+    x0 = getInitialState(model);
+    q0 = x0(1:model.n);
 
     %% Pick control
 
-    control = 3;    % 1-3
+    control = 4;    % 1-3
 
     switch control
         case 1
@@ -60,32 +62,40 @@ function tau = studentController(t, s, model, params)
             
         case 4
             % Force control
-            %   COMMMENTS
-            %   COMMMENTS
-            %   COMMMENTS
-            %   COMMMENTS
-            %   COMMMENTS
-            %   COMMMENTS
-            %   COMMMENTS
-            %   COMMMENTS
-            1+2
-            3+4
+            wren = wrench_gen(q, dq, q0);
             
         otherwise
             warning('Control not recognized.')
     end
 end
 
+
 %% Generation of Stabilizing Wrench
 function [wrench] = wrench_gen(q, dq, q0)
-    m = 31; 
-    g = -9.81; kp_f = 200 ; kd_f = 20 ; kp_t = 200 ; kd_t = 20 ;
+    m = 31; g = -9.81; kp_f = 200 ; kd_f = 20 ;
 
-
-    fd_r = -kp_f*(q(1:3)-q0(1:3)) - kd_f*dq(1:3) ;
-    fd_GA = [0, 0, -mg]' + fd_r;
+    %reaction force to correct translational position deviation
+    fd_r = -kp_f*(q(1:3)-q0(1:3)) - kd_f*dq(1:3) ;  
+    fd_GA = [0, 0, m*g]' + fd_r;  %total force component in the wrench
        
-    tau = -kp_t*(dq(4:6)-zeros(3,1)) - kd_t*ddq(4:6) ;
+    eul = q(4:6)';  %Euler angle of the body (roll, pitch, yaw in a row vector)
+    quat = eul2quat(eul,'XYZ');
+    del = quat(1); eps = quat(2:4)';
+    kp_t = 100;
+    K_r = kp_t*eye(3);  
+    %3x3 diagnoal matrix to represent stiffness of torsional spring
+    %controlling roll pitch and yaw
     
-    wrench = [fd_GA; tau];
+    t_r = -2*(del*eye(3) + eps./norm(eps))*K_r*eps;
+    
+    R_wb = eul2rotm(eul,'XYZ');
+    kd_t = 50;
+    D_r = kd_t*eye(3);  %3x3 symmetric positive matrix to represent damping
+    w = dq(4:6);  %small angle approximation  
+    
+    td_GA = R_wb*(t_r - D_r*(w - zeros(3,1))); 
+    %R_wb is transformation matrix from body to world
+    %w_d = zeros(3,1)
+    
+    wrench = [fd_GA; td_GA];
 end
